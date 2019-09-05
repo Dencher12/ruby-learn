5d4a28c99ffaff4dec600a7983c3bd419439b3ea
Dencher12 <37984810+Dencher12@users.noreply.github.com>
2019-09-05 20:34:41 +0300

    Revert "Revert "Update Station.rb""
    This reverts commit fdf849f258550626b198244f29c5e743f6824b4d.

--- lesson3/Station.rb
+++ lesson3/Station.rb
@@ -29,7 +29,26 @@ class Station
   end
 end
 
-class Train 
+class Route
+  attr_reader :stations, :name
+
+  def initialize(firts_station, last_station)
+    @stations = [firts_station, last_station]
+    @name = "#{firts_station.name} - #{last_station.name}"
+  end
+
+  def add_station(station)
+    @stations.insert(-2, station)
+  end
+
+  def delete_station(station)
+    if station != @stations.last && station != @stations.first
+      @stations.delete(station)
+    end
+  end
+end
+
+class Train
   attr_reader :id, :type, :car_quantity, :speed, :station_index
 
   def initialize(id, type, car_quantity = 0)
@@ -66,7 +85,7 @@ class Train
   end
 
   def take_route(route)
-    puts "Поезд #{id}: установлен маршрут #{route.stations[0].name} - #{route.stations[-1].name}"
+    puts "Поезд #{id}: установлен маршрут #{route.name}"
     @route = route
     @station_index = 0
     route.stations[0].take_train(self)
@@ -89,39 +108,24 @@ class Train
   end
 
   def current_station
-    puts "Поезд #{id}: текущая остановка - #{@route.stations[@station_index].name}"
+    station = @route.stations[@station_index].name
+    puts "Поезд #{id}: текущая остановка - #{station}"
     @route.stations[@station_index]
   end
 
   def next_station
     if current_station != @route.stations.last
-      puts "Поезд #{id}: следущая остановка - #{@route.stations[@station_index+1].name}"
+      station = @route.stations[@station_index + 1].name
+      puts "Поезд #{id}: следущая остановка - #{station}"
       @route.stations[@station_index + 1]
     end
   end
 
   def prev_station
     if current_station != @route.stations.first
-      puts "Поезд #{id}: предидущая остановка - #{@route.stations[@station_index - 1].name}"
+      station = @route.stations[@station_index - 1].name
+      puts "Поезд #{id}: предидущая остановка - #{station}"
       @route.stations[@station_index - 1]
-    end
-  end
-end
-
-class Route
-  attr_reader :stations
-
-  def initialize(firts_station, last_station)
-    @stations = [firts_station, last_station]
-  end
-
-  def add_station(station)
-    @stations.insert(-2, station)
-  end
-
-  def delete_station(station)
-    if station != @stations.last && station != @stations.first
-      @stations.delete(station)
     end
   end
 end
