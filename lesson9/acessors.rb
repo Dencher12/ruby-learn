4707389a4c3a06359a4bf8a3707103d80b46f5c0
Den <deischerry19@gmail.com>
2019-09-29 00:20:11 +0300

    lesson9: добавил strong_attr_acessor

--- lesson9/acessors.rb
+++ lesson9/acessors.rb
@@ -6,26 +6,31 @@ module Acessors
       @hist ||= []
       instance_variable_set(var_name, val)
       @hist << val
-      #create_hist_getter(name)
       define_singleton_method("#{name}_history") { @hist }
     end
   end
 
-  private
-
-  def create_hist_getter(name)
-    define_method("#{name}_history") { @hist }
+  def strong_attr_acessor(name, type)
+    var_name = "@#{name}".to_sym
+    define_method(name) { instance_variable_get(var_name) }
+    define_method("#{name}=".to_sym) do |val|
+      if val.class == type
+        instance_variable_set(var_name, val)
+      else
+        puts "Значение дожно быть типа #{type}"
+      end
+    end
   end
 end
 
 class Test
   extend Acessors
-  attr_accessor_with_history :s
+  strong_attr_acessor :s, String
 end
 
 t = Test.new
 t.s = 10
 t.s = 5
-t.s = 3
+t.s = "75"
 
 puts t.s
